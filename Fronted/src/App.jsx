357629971ea1b3214cb6f837fd7ccd39a6b96618
Dhanjee Gond <dgond62063@gmail.com>
2026-01-20 16:33:59 +0530

    changes

--- Fronted/src/App.jsx
+++ Fronted/src/App.jsx
@@ -3,7 +3,7 @@ import Try from "./try/try";
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to StyleSwap</h1>
+      <h1>Welcome to StyleSwap React.js</h1>
 
       <Try />
     </div>
